c8baafc16a91133fe439b281966bee158b6fc558
Courtney Spitzer <spitzerc22@gmail.com>
2022-01-16 13:47:05 -0600

    definitly had struggle with this, will return, but trying to catch up

--- src/MoleContainer.js
+++ src/MoleContainer.js
@@ -2,19 +2,31 @@
 import {useState} from 'react'
 import React from 'react'
 import Mole from './Mole'
+import EmptySlot from './EmptySlot'
 
 //HANDLECLICK FUNC - SCORE BY 1 AND TERNARY FOR WHICH CHILD TO RENDER
 
 function MoleContainer (props) {
     let [displayMole, setDisplayMole] = useState(false)
 
-    const moleBopped = () => {
-        //having trouble with this, will reach out for help
+    const moleBopped = (e) => {
+        props.setScore(props.score + 1)
+        setDisplayMole(false)
     }
+
+    const shownMole = displayMole ?
+    <Mole   
+        clickHandler={moleBopped}
+        setScore={props.setScore}
+        toggle={setDisplayMole}
+    /> 
+    :
+    <EmptySlot toggle={setDisplayMole} />
+
     return(
         <div>
                 <h1>Mole Container</h1>
-                <Mole />
+                {shownMole}
         </div>
     )
 }
